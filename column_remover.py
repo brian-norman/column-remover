e254e530c62614d1be271a588e93f8cdc40a334e
Brian Norman <briankn8@gmail.com>
2017-04-03 09:28:27 -0400

    Add multiple columns and delimeter

--- column_remover.py
+++ column_remover.py
@@ -1,12 +1,14 @@
 import sys
 
-def read_file(file_in_name, index_to_remove, number_of_header_lines):
+def read_file(file_in_name, index_to_remove, number_of_header_lines, seperated_by):
 	file = open(file_in_name)
 	lines = file.readlines()
 	list_of_lines = []
+
 	for line in lines[number_of_header_lines:]:
-		lis = line.split()
-		lis.pop(index_to_remove)
+		lis = line.split(seperated_by)
+		for index in reversed(index_to_remove):
+			lis.pop(index)
 		new_line = " ".join(lis)
 		list_of_lines.append(new_line)
 	file.close()
@@ -15,20 +17,28 @@ def read_file(file_in_name, index_to_remove, number_of_header_lines):
 def write_file(file_out_name, list_of_lines):
 	file = open(file_out_name, "w")
 	for line in list_of_lines:
-		file.write(line + '\n')
+		file.write(line)
 	file.close()
 
 def main():
 	file_in_name = input("Old file name: ")
 	file_out_name = input ("New file name: ")
-	index_to_remove = int(input("Column to be removed: ")) - 1
 	number_of_header_lines = int(input("Number of header lines to ignore: "))
+	seperated_by = input("Data seperated by (if space, enter space): ")
+	
+	index_to_remove = input("Column to be removed (if multiple, seperate by space, ascending order): ")
+	if len(index_to_remove) > 1:
+		index_to_remove = index_to_remove.split()
+		for i, index in enumerate(index_to_remove):
+			index_to_remove[i] = int(index) - 1
+	elif len(index_to_remove) == 1:
+		index_to_remove = [int(index_to_remove) - 1]
 
-	list_of_lines = read_file(file_in_name, index_to_remove, number_of_header_lines)
+	list_of_lines = read_file(file_in_name, index_to_remove, number_of_header_lines, seperated_by)
 	write_file(file_out_name, list_of_lines)
 
-main()
+if __name__ == "__main__":
+	main()
 
 # TO-DO
-# Add ability to enter multiple columns
-# Add ability to choose what the data is seperated by (commas, spaces, anything)
+# Add ability to include header lines in output file or not
